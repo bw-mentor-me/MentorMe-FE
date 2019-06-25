e2ca9cfb7afa355e79e825d123a8c8375ff843fe
Karl Beksa <karlbeksa@gmail.com>
2019-06-25 13:43:11 -0400

    for reclone

--- mentor-app/src/components/Signup.js
+++ mentor-app/src/components/Signup.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class Signup extends Component {
+class Signup extends Component {
     state = {
         newUser: {
             username: '',
@@ -43,10 +43,13 @@ export default class Signup extends Component {
                     <input type='text' name='username' placeholder='username' value={this.state.username} onChange={this.handleChanges} />
                     <input type='password' name='password' placeholder='password' value={this.state.password} onChange={this.handleChanges} />
                     <input type='text' name='phoneNumber' placeholder='Cell Phone Number' value={this.state.phoneNumber} onChange={this.handleChanges} />
-                    <input type='text' name='industryType' placeholder='Industry' value={this.state.industryType} onChange={this.handleChanges} />
-                   
+                    <input type='text' name='industryType' placeholder='Industry' value={this.state.industryType} onChange={this.handleChanges} />        
                 </form>
             </div>
         )
     }
 }
+
+
+export default Signup;
+
